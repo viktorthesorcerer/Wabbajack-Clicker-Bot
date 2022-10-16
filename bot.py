24f58473a60876399b64cb51603890149b0599c4
viktorthesorcerer <92230565+viktorthesorcerer@users.noreply.github.com>
2022-10-16 19:15:03 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import time
+import keyboard
+import pyautogui
+import win32api
+import win32con
+
+time.sleep(5)
+
+
+def click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    time.sleep(0.1)
+
+
+while not keyboard.is_pressed('q'):
+    start = pyautogui.locateCenterOnScreen('image.png', region=(0, 0, 1920, 1080), grayscale=True, confidence=0.70)
+    if start is not None:
+        pyautogui.moveTo(start)  # Moves the mouse to the coordinates of the image
+        click()
